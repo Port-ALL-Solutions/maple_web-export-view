e772bf70545285ec3c92716b3e53a662de4d5830
Benoît Vézina <benoit@vezina.biz>
2017-03-30 12:37:34 -0400

    removed header

---
+++ controllers/controllers.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Henry Zhou (http://www.maxodoo.com)
+# Copyright 2016 Rodney (http://clearcorp.cr/)
+# Copyright 2012 Agile Business Group
+# Copyright 2012 Therp BV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import re
+import json
+import odoo.http as http
+from odoo.http import request
+from odoo.addons.web.controllers.main import ExcelExport
+from odoo.tools.misc import str2bool, xlwt
+from cStringIO import StringIO
+
+class ExcelExportView(ExcelExport):
+    def __getattribute__(self, name):
+        if name == 'fmt':
+            raise AttributeError()
+        return super(ExcelExportView, self).__getattribute__(name)
+
+    @http.route('/web/export/xls_view', type='http', auth='user')
+    def export_xls_view(self, data, token):
+        data = json.loads(data)
+        model = data.get('model', [])
+        columns_headers = data.get('headers', [])
+        rows = data.get('rows', [])
+
+        return request.make_response(
+            self.from_data(columns_headers, rows),
+            headers=[
+                ('Content-Disposition', 'attachment; filename="%s"'
+                 % self.filename(model)),
+                ('Content-Type', self.content_type)
+            ],
+            cookies={'fileToken': token}
+        )
+
+    def from_data(self, fields, rows):
+        workbook = xlwt.Workbook()
+        worksheet = workbook.add_sheet('Sheet 1')
+
+        base_style = xlwt.easyxf('align: wrap yes')
+        date_style = xlwt.easyxf('align: wrap yes', num_format_str='YYYY-MM-DD')
+        datetime_style = xlwt.easyxf('align: wrap yes', num_format_str='YYYY-MM-DD HH:mm:SS')
+
+        for row_index, row in enumerate(rows):
+            for cell_index, cell_value in enumerate(row):
+                cell_style = base_style
+                if isinstance(cell_value, basestring):
+                    cell_value = re.sub("\r", " ", cell_value)
+                elif isinstance(cell_value, datetime.datetime):
+                    cell_style = datetime_style
+                elif isinstance(cell_value, datetime.date):
+                    cell_style = date_style
+                worksheet.write(row_index, cell_index, cell_value, cell_style)
+
+        fp = StringIO()
+        workbook.save(fp)
+        fp.seek(0)
+        data = fp.read()
+        fp.close()
+        return data
